0f49f88724a74ee958e2466d7398597c713ed752
Huynh Tai <tai0898235536@gmail.com>
2025-05-21 18:55:04 +0700

    21/5/2025

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import connectDB from "./configs/db.js";
+
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ credentials: true }));
+
+app.get("/", (req, res) => res.send("API Working"));
+
+app.listen(port, () => console.log(`Server running on PORT: ${port}`));
